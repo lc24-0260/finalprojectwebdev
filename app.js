8737f9d65d19b4d7755d9a0e1d902462c370b811
Tsimpolos <panostsi05@gmail.com>
2026-04-28 13:08:26 -0700

    Javascript changes

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
-// console.log("Start!")
-
-// setTimeout(()=>{
-
-// }, 2000);
-
-
-// console.log("Finish!")
-
 const http = require("http");
 const fs = require("fs");
+const path = require("path");
+
+const MIME_TYPES = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "application/javascript",
+    ".json": "application/json",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg",
+    ".svg": "image/svg+xml"
+};
 
 const server = http.createServer((req, res) => {
-    fs.readFile("index.html", (err,data) => {
-        res.writeHead(200,{"Content-Type":"text/html"});
-        res.write(data);
-        res.end();
-    })
+    let filePath = req.url === "/" ? "index.html" : req.url.replace(/^\//, "");
+    filePath = path.join(__dirname, filePath);
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("404 - File not found");
+            return;
+        }
+
+        const ext = path.extname(filePath).toLowerCase();
+        const contentType = MIME_TYPES[ext] || "application/octet-stream";
+
+        res.writeHead(200, { "Content-Type": contentType });
+        res.end(data);
+    });
 });
 
-
 server.listen(3000, () => {
-    console.log("Server runnng on http:localhost:3000")
-})
+    console.log("Server running on http://localhost:3000");
+});
